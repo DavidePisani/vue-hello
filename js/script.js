67d27d70f912f6e1cb418453aa57c91b6eecc20c
DavidePisani <davide.pisani96@gmail.com>
2022-06-23 13:17:21 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// BONUS:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data (in data avrò l'src).
+
+var app = new Vue({
+    el: '#root',
+    data: {
+      message: 'Hei ciaooo sono la prima scimmia su VUE',
+      Image: 'https://www.repstatic.it/content/nazionale/img/2017/09/12/095727836-c36c7100-6020-40fd-ab27-f58df42b576c.jpg'
+    }
+  }) 
+  
